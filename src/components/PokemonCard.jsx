e2d5f3b42980b21a90eb511400309c6c2fa244b7
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2025-10-15 10:29:20 -0600

    Adding abilities to card

--- src/components/PokemonCard.jsx
+++ src/components/PokemonCard.jsx
@@ -3,13 +3,15 @@ import { Card } from'antd';
 import Meta from 'antd/es/card/Meta';
 
 
-const PokemonCard = ({name, image})=>{
+const PokemonCard = ({name, image, abilities})=>{
+    const abilitiesName = abilities?.map(a=>a.ability.name);
+    const abilitiesResume = abilitiesName.join(',');
     return <Card
         title={name}
         cover={<img src={image} alt={name} />}
         extra={<StarOutlined />}
     >
-        <Meta description="fire, magic"/>
+        <Meta description={abilitiesResume}/>
     </Card>
 }
 
--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -5,7 +5,12 @@ const PokemonList = ({ pokemons=Array(10).fill('') }) => {
     return (
         <div className="PokemonList"> 
             {pokemons.map(pokemon => {
-                return <PokemonCard name={pokemon.name} key={pokemon.name} image={pokemon.sprites.front_default} />;
+                return <PokemonCard 
+                    name={pokemon.name} 
+                    key={pokemon.name} 
+                    image={pokemon.sprites.front_default} 
+                    abilities={pokemon.abilities}
+                />;
             })}
         </div>
     );
