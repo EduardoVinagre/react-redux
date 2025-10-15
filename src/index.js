c94f04d9f99c9537c4073ac7cb08104171e1e4b3
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2025-10-15 09:25:31 -0600

    Add middleware examples

--- src/index.js
+++ src/index.js
@@ -1,16 +1,23 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
-import "./index.css";
+import { Provider } from "react-redux";
+import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
 import App from "./App";
 import { pokemonsReducer } from "./reducers/pokemons";
-import { Provider } from "react-redux";
-import { legacy_createStore as createStore } from "redux";
+import { featuring, logger } from './middlewares';
+import "./index.css";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
+const composedEnhancers = compose(
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__(),
+  applyMiddleware(logger),
+  applyMiddleware(featuring)
+)
+
 const store = createStore(
   pokemonsReducer,
-  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+  composedEnhancers
 );
 
 root.render(
