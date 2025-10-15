1986153fcc73a00eb26abde67ecb93a5f848b8e9
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2025-10-15 08:56:02 -0600

    Adding enhancer for redux devtools

--- src/index.js
+++ src/index.js
@@ -1,14 +1,17 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import { pokemonsReducer } from './reducers/pokemons';
-import { Provider } from 'react-redux';
-import { legacy_createStore as createStore } from 'redux';
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import { pokemonsReducer } from "./reducers/pokemons";
+import { Provider } from "react-redux";
+import { legacy_createStore as createStore } from "redux";
 
-const root = ReactDOM.createRoot(document.getElementById('root'));
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-const store = createStore(pokemonsReducer);
+const store = createStore(
+  pokemonsReducer,
+  window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+);
 
 root.render(
   <React.StrictMode>
