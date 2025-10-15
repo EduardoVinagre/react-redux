48245a6abb396d1c06b404670781d5ab947be8af
José Eduardo Vinagre de Dios <eduardovinagre@gmail.com>
2025-10-15 14:19:31 -0600

    Feat: Mark as favorite. Put the types of each pokemon

--- src/components/PokemonList.jsx
+++ src/components/PokemonList.jsx
@@ -5,7 +5,13 @@ const PokemonList = ({ pokemons=Array(10).fill('') }) => {
     return (
         <div className="PokemonList"> 
             {pokemons.map(pokemon => {
-                return <PokemonCard name={pokemon.name} key={pokemon.name} image={pokemon.sprites.front_default} />;
+                return <PokemonCard 
+                    name={pokemon.name} 
+                    key={pokemon.name} 
+                    image={pokemon.sprites.front_default} 
+                    types={pokemon.types}
+                    id={pokemon.id}
+                    favorite={pokemon.favorite} />;
             })}
         </div>
     );
--- src/reducers/pokemons.js
+++ src/reducers/pokemons.js
@@ -1,4 +1,4 @@
-import { SET_LOADING, SET_POKEMONS } from "../actions/types"
+import { SET_FAVORITE, SET_LOADING, SET_POKEMONS } from "../actions/types"
 
 const initialState = {
   pokemons: [],
@@ -11,6 +11,20 @@ export const pokemonsReducer = (state = initialState, action) => {
       return {
         ...state, pokemons: action.payload
       };
+    case SET_FAVORITE:
+      const newPokemonsList = [...state.pokemons];
+      const currentPokemonIndex = newPokemonsList.findIndex(pokemon=>pokemon.id === action.payload.pokemonId)
+
+      if(currentPokemonIndex < 0){
+        return state;
+      }
+
+      newPokemonsList[currentPokemonIndex].favorite = !newPokemonsList[currentPokemonIndex].favorite;
+
+      return {
+        ...state, 
+        pokemons:newPokemonsList
+      }
     case SET_LOADING:
       return {
         ...state, loading: action.payload
