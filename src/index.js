14508a7b504edd495cabc4058f7e670530c7fbf4
eduardo vinagre <eduardovinagre@gmail.com>
2025-10-16 16:31:16 -0600

    Use combineReducers, install redux-immutable

--- src/index.js
+++ src/index.js
@@ -3,10 +3,10 @@ import ReactDOM from "react-dom/client";
 import { Provider } from "react-redux";
 import { applyMiddleware, compose, legacy_createStore as createStore } from "redux";
 import App from "./App";
-import { pokemonsReducer } from "./reducers/pokemons";
 import { thunk } from "redux-thunk";
 import { logger } from './middlewares';
 import "./index.css";
+import rootReducer from "./reducers/rootReducer";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 
@@ -17,7 +17,7 @@ const composedEnhancers = composeAlt(
 )
 
 const store = createStore(
-  pokemonsReducer,
+  rootReducer,
   composedEnhancers
 );
 
